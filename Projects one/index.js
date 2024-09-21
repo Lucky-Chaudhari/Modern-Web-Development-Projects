a12c5f61a6a8a0f60027734697999606648b890e
Lucky Chaudhari <luckychaudhari014@gmail.com>
2024-09-21 23:24:37 +0530

    code is update

--- Projects one/index.js
+++ Projects one/index.js
@@ -42,44 +42,78 @@ function firstPageAnim() {
 }
 
 
-function circleMouseFollower(){
-  window.addEventListener("mousemove",function(dets){
-       document.querySelector(".mini-circle").style.transform=`translate(${dets.clientX}px,${dets.clientY}px)`;
+// jab mouse move ho to hum skew kar payye aur maximum skew and minimum skew define kar paaye
+
+function circleSkew(){
+
+   //define default scale value
+
+  //x & y current value
+  var xscale = 1;
+  var yscale = 1;
+
+  //x & y previous value
+  var xprev = 0;
+  var yprev = 0;
+
+   window.addEventListener("mousemove",function(dets){
+    this.clearTimeout(timeout);
+    xscale = gsap.utils.clamp(.8, 1.2, dets.clientX - xprev);
+    yscale = gsap.utils.clamp(.8, 1.2, dets.clientY - yprev );
+
+     xprev = dets.clientX;
+     yprev = dets.clientY;
+
+     circleMouseFollower(xscale, yscale);
+
+       timeout = setTimeout(function(){
+        document.querySelector(
+                  ".mini-circle"
+                ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+       },100);
+   });
+
+}
+
+function circleMouseFollower(xscale, yscale){
+  window.addEventListener("mousemove",function (dets){
+       document.querySelector(".mini-circle").style.transform= `translate(${dets.clientX}px,${dets.clientY}px)scale(${xscale},${yscale})`;
   })
 
 }
 circleMouseFollower();
  firstPageAnim();
-
-
-
- // jab mouse move ho to hum skew kar payye aur maximum skew and minimum skew define kar paaye
-
- function circleSkew(){
-  var xscale = 1;
-  var yscale = 1;
-
-  var xprev = 0;
-  var yprev = 0;
-
-  window.addEventListener("mousemove", function (dets) {
-    clearTimeout(timeout);
-
-    xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
-    yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
-
-    xprev = dets.clientX;
-    yprev = dets.clientY;
-
-    circleMouseFollower(xscale, yscale);
-
-    timeout = setTimeout(function () {
-      document.querySelector(
-        ".mini-circle"
-      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
-    }, 100);
-  });
- }
-
-
  circleSkew();
+
+
+
+
+//we have select the three elem and then to applyed the mousemove and find the mouse position  where the mouse is presents
+//  we finding mouse position with the help of xpositio and y position  and showing the img  In Place Of  of x y position and move that img
+
+
+document.querySelectorAll(".elem").forEach(function (elem) {
+  var rotate = 0;
+  var diffrot = 0;
+
+  elem.addEventListener("mouseleave", function (dets) {
+    gsap.to(elem.querySelector("img"), {
+      opacity: 0,
+      ease: Power3,
+      duration: 0.5,
+    });
+  });
+
+  elem.addEventListener("mousemove", function (dets) {
+    var diff = dets.clientY - elem.getBoundingClientRect().top;
+    diffrot = dets.clientX - rotate;
+    rotate = dets.clientX;
+    gsap.to(elem.querySelector("img"), {
+      opacity: 1,
+      ease: Power3,
+      top: diff,
+      left: dets.clientX,
+      rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+    });
+  });
+});
